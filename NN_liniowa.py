8a72055f193148d4bdd08c2dbf469488234ace4f
Piotr Gorzelnik <gorzelnik.p@gmail.com>
2023-03-26 20:26:00 +0200

    added numpy linear function for comparison

--- NN_liniowa.py
+++ NN_liniowa.py
@@ -4,10 +4,10 @@ import numpy as np
 def square(x):
     return x**2
 
-x_values = np.arange(25)
+x_values = np.arange(100)
 y_values = [square(x) for x in x_values]
 
-interp_x = np.arange(0, 25, 6) # range of x values to interpolate at
+interp_x = np.arange(0, 100, 24.5) # range of x values to interpolate at
 interp_y = []
 
 # linear interpolation
@@ -22,6 +22,9 @@ for x in interp_x:
     interp_y.append(y)
 
 
+interp_y_numpy = np.interp(interp_x, x_values, y_values)
+
+#git statusplt.plot(interp_x, interp_y_numpy, label='Linear interpolation - numpy')
 plt.plot(interp_x, interp_y, label='Linear interpolation')
 plt.plot(x_values, y_values, '.', label='Square function')
 plt.legend()
